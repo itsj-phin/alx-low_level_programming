fe7573549c735975a7a4e0a36c1e5cee88692fc8
itsj-phin <josephineoketchW@gmail.com>
2022-09-14 10:35:14 -0700

    Alphabet insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+**/
+
+void print_alphabet(void)
+{
+char alphy;
+for (alphy = 'a'; alphy <= 'z'; alphy++)
+{
+putchar(alphy);
+}
+
+_putchar('\n');
+
+}
